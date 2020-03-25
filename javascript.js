2771918f856cd4b6a4838e75117a2e94d11d7788
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-03-25 17:47:29 -0500

    fixed time,peusdo code,start on time blocks

--- javascript.js
+++ javascript.js
@@ -1,5 +1,41 @@
-// $("current day")=moment().format('MMMM Do YYYY, h:mm:ss a');
-// m = moment('2013-03-01', 'YYYY-MM-DD')
-var actualDay= ("currentDay")
+// THEN the current day is displayed at the top of the calendar
 
-actualDay.text("currentDay").format('MMMM Do YYYY, h:mm:ss a');
+const now = moment().format('MMMM Do YYYY');
+let $dateHeading = $('#currentDay');
+  $dateHeading.text(now);
+
+
+
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+function timeBlocks(){
+    var hourBlock = $('<div class=')
+    var entryBlock
+    var saveBlock
+}
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+//  if (put in code for past){
+    //  then put color here.
+//  } 
+
+// if (time is in the present)
+
+// if (time is in the future)
+ 
+
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+// input box for event box
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+// event listner(.on) save to local storage
+
+// WHEN I refresh the page
+// THEN the saved events persist
