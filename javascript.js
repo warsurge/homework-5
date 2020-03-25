216a560fa010be93b01edaaecff8c9678d32636a
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-03-25 15:43:52 -0500

    added time and date

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// $("current day")=moment().format('MMMM Do YYYY, h:mm:ss a');
+// m = moment('2013-03-01', 'YYYY-MM-DD')
+var actualDay= ("currentDay")
+
+actualDay.text("currentDay").format('MMMM Do YYYY, h:mm:ss a');
