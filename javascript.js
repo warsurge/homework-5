ce37d2130e7164ed7255309e41d04493f7e11af1
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-03-26 18:34:34 -0500

    added the colors to the boxes

--- javascript.js
+++ javascript.js
@@ -1,30 +1,140 @@
 
-var crClockInit1 = null;
-var crClockInterval = null
-function crStartClockNow() {
-    crClockInterval = setInterval(function () {
-        $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+var clockInit1 = null;
+var clockInterval = null
+function startClockNow() {
+    clockInterval = setInterval(function () {
+        $('#currentDay').html(moment().format('MMMM Do YYYY, H:mm:ss a'));
     }, 1000);
 }
-$(crStartClockNow).text('#currentDay');
+$(startClockNow).text('#currentDay');
 
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-function timeBlocks() {
-
-}
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-//  if (put in code for past){
-    //  then put color here.
-//  } 
+function checkUpdateColors() {
+    var d = moment();
+    var hourCompare = moment().format("HH");
 
-// if (time is in the present)
+    if ((hourCompare == 8)) {
+        $("#row1").addClass("present");
+    }
+    else if ((hourCompare >= 8)) {
+        $("#row1").addClass("past");
+    }
+    else {
+        $("#row1").addClass("future");
+    }
+    if ((hourCompare == 9)) {
+        $("#row2").addClass("present");
+    }
+    else if ((hourCompare >= 9)) {
+        $("#row2").addClass("past");
+    }
+    else {
+        $("#row2").addClass("future");
+    }
+    if ((hourCompare == 10)) {
+        $("#row3").addClass("present");
+    }
+    else if ((hourCompare >= 10)) {
+        $("#row3").addClass("past");
+    }
+    else {
+        $("#row3").addClass("future");
+    }
+    if ((hourCompare == 11)) {
+        $("#row4").addClass("present");
+    }
+    else if ((hourCompare >= 11)) {
+        $("#row4").addClass("past");
+    }
+    else {
+        $("#row4").addClass("future");
+    }
+    if ((hourCompare == 12)) {
+        $("#row5").addClass("present");
+    }
+    else if ((hourCompare >= 12)) {
+        $("#row5").addClass("past");
+    }
+    else {
+        $("#row5").addClass("future");
+    }
+    if ((hourCompare == 13)) {
+        $("#row6").addClass("present");
+    }
+    else if ((hourCompare >= 13)) {
+        $("#row6").addClass("past");
+    }
+    else {
+        $("#row6").addClass("future");
+    }
+    if ((hourCompare == 14)) {
+        $("#row7").addClass("present");
+    }
+    else if ((hourCompare >= 14)) {
+        $("#row7").addClass("past");
+    }
+    else {
+        $("#row7").addClass("future");
+    }
+    if ((hourCompare == 15)) {
+        $("#row8").addClass("present");
+    }
+    else if ((hourCompare >= 15)) {
+        $("#row8").addClass("past");
+    }
+    else {
+        $("#row8").addClass("future");
+    }
+    if ((hourCompare == 16)) {
+        $("#row9").addClass("present");
+    }
+    else if ((hourCompare >= 16)) {
+        $("#row9").addClass("past");
+    }
+    else {
+        $("#row9").addClass("future");
+    }
+    if ((hourCompare == 17)) {
+        $("#row10").addClass("present");
+    }
+    else if ((hourCompare >= 17)) {
+        $("#row10").addClass("past");
+    }
+    else {
+        $("#row10").addClass("future");
+    }
+    if ((hourCompare == 18)) {
+        $("#row11").addClass("present");
+    }
+    else if ((hourCompare >= 118)) {
+        $("#row11").addClass("past");
+    }
+    else {
+        $("#row11").addClass("future");
+    }
+    if ((hourCompare == 19)) {
+        $("#row12").addClass("present");
+    }
+    else if ((hourCompare >= 19)) {
+        $("#row12").addClass("past");
+    }
+    else {
+        $("#row12").addClass("future");
+    }
+    if ((hourCompare == 20)) {
+        $("#row13").addClass("present");
+    }
+    else if ((hourCompare >= 20)) {
+        $("#row13").addClass("past");
+    }
+    else {
+        $("#row13").addClass("future");
+    }
+}
 
-// if (time is in the future)
-
+checkUpdateColors();
 
 
 // WHEN I click into a timeblock
